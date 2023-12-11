ec9addbc53585d5ec0939a6b315f4de01bfc112b
anton <antonkuchisnky86@gmail.com>
2023-12-11 18:06:04 +0300

    add voting service

---
+++ voting-service/src/main/java/com/exercise/kameleoon/votingservice/VotingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.kameleoon.votingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingServiceApplication.class, args);
+    }
+
+}
