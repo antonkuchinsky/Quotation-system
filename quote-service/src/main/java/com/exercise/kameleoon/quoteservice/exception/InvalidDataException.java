74515cae27d9ade55520280e4144efe931e9948a
anton <antonkuchisnky86@gmail.com>
2023-12-11 14:50:12 +0300

    created invalid data exception

---
+++ quote-service/src/main/java/com/exercise/kameleoon/quoteservice/exception/InvalidDataException.java
@@ -0,0 +1,15 @@
+package com.exercise.kameleoon.quoteservice.exception;
+
+public class InvalidDataException extends RuntimeException{
+
+    private final String title;
+
+    public InvalidDataException(String message, String title) {
+        super(message);
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
