932f88276417926a19e443220d2ce86911306a28
anton <antonkuchisnky86@gmail.com>
2023-12-11 18:00:42 +0300

    add model vote and jpa repository

---
+++ voting-service/src/main/java/com/exercise/kameleoon/votingservice/repository/VoteRepository.java
@@ -0,0 +1,15 @@
+package com.exercise.kameleoon.votingservice.repository;
+
+import com.exercise.kameleoon.votingservice.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, UUID> {
+    Optional<List<Vote>> getAllByQuoteId(UUID quoteId);
+    Optional<Vote> findByQuoteIdAndUsername(UUID quoteId, String username);
+}
