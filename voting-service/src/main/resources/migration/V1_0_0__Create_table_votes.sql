3a694b8aa34806fd1c8cac277470746bd86001d1
anton <antonkuchisnky86@gmail.com>
2023-12-12 17:57:46 +0300

    refactoring code

--- voting-service/src/main/resources/migration/V1_0_0__Create_table_votes.sql
+++ voting-service/src/main/resources/migration/V1_0_0__Create_table_votes.sql
@@ -1,7 +1,7 @@
 create table if not exists votes
 (
     id           uuid,
-    quote_id   uuid unique not null,
+    quote_id   uuid not null,
     username      varchar(30) not null unique ,
     in_favor  boolean not null,
     primary key (id)
