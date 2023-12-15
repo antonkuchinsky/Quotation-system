914d6e899af6eb2ee00f213848ace9d6e7c3d8b0
anton <antonkuchisnky86@gmail.com>
2023-12-15 19:03:34 +0300

    refactoring schema

--- voting-service/src/main/resources/migration/V1_0_0__Create_table_votes.sql
+++ voting-service/src/main/resources/migration/V1_0_0__Create_table_votes.sql
@@ -2,7 +2,7 @@ create table if not exists votes
 (
     id           uuid,
     quote_id   uuid not null,
-    username      varchar(30) not null unique ,
+    username      varchar(30) not null ,
     in_favor  boolean not null,
     primary key (id)
     );
