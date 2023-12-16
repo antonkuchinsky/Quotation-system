a46a5425c7547b0f2358db2cb0eb4add5000569c
anton <antonkuchisnky86@gmail.com>
2023-12-16 13:11:59 +0300

    added password encryption

--- user-service/src/main/java/com/exercise/kameleoon/userservice/mapper/UserRegisterMapperDto.java
+++ user-service/src/main/java/com/exercise/kameleoon/userservice/mapper/UserRegisterMapperDto.java
@@ -2,6 +2,7 @@ package com.exercise.kameleoon.userservice.mapper;
 
 import com.exercise.kameleoon.userservice.dto.UserRegisterDto;
 import com.exercise.kameleoon.userservice.model.User;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -12,10 +13,11 @@ public class UserRegisterMapperDto implements Function<UserRegisterDto,User> {
 
     @Override
     public User apply(UserRegisterDto userRegisterDto) {
+        String hashedPassword = BCrypt.hashpw(userRegisterDto.password(), BCrypt.gensalt());
         return User.builder()
                 .email(userRegisterDto.email())
                 .username(userRegisterDto.username())
-                .password(userRegisterDto.password())
+                .password(hashedPassword)
                 .dateCreate(LocalDate.now()).build();
     }
 }
